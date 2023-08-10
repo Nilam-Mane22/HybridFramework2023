1573c40625a617e245469eb6e20594080c50983d
Nilam <mane.nilam12@gmail.com>
2023-08-10 13:21:52 +0530

    Added Category test

--- src/test/java/com/learnAutomation/testcases/ManageCategoryTest.java
+++ src/test/java/com/learnAutomation/testcases/ManageCategoryTest.java
@@ -1,11 +1,11 @@
 package com.learnAutomation.testcases;
 import org.testng.annotations.Test;
 
-
 import com.learnautomation.dataProvider.CustomDataProvider;
 import com.learnautomation.helper.BaseClass;
 import com.learnautomation.pages.LoginPage;
 import com.learnautomation.pages.ManageCategoryPage;
+
 
 public class ManageCategoryTest extends BaseClass
 {
@@ -17,7 +17,7 @@ public class ManageCategoryTest extends BaseClass
 	}
 	
 	
-	  @Test(priority = 2, dataProvider = "category", dataProviderClass = CustomDataProvider.class)
+	  @Test(priority = 4, dataProvider = "category", dataProviderClass = CustomDataProvider.class)
       public void categoryManage(String categoryName) throws InterruptedException
       {		   
               ManageCategoryPage Mcategory = new ManageCategoryPage(driver);             
